952c738e6e6e3f8fff956745a6fe04ed72f1f113
Prakkks <tprakrity@gmail.com>
2024-06-02 16:14:23 +0545

    added model:product and make basic for shop ui

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'shop'
+    'blog.apps.BlogConfig',
+    'shop.apps.ShopConfig'
+
 ]
 
 MIDDLEWARE = [
