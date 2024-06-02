4b3413add0d0fac471081ebc685cd944d73c7c61
Prakkks <tprakrity@gmail.com>
2024-06-02 16:50:10 +0545

    Added models

--- shop/models.py
+++ shop/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    product_id =models.AutoField
-    product_name = models.CharField(max_length=50)
-    desc=models.CharField(max_length=300)
-    pub_date =models.DateField()
     
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=50)
+    category =models.CharField(max_length =50, default='')
+    subcategory = models.CharField(max_length=50, default='')
+    price =models.IntegerField(default=100)
+    pub_date = models.DateField()
+    desc=models.CharField(max_length=300)
+    image = models.ImageField(upload_to="shop/images/", default='')
+
 
     
